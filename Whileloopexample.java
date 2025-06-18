a4257cf1c7a0ab6138224606e347d9e0cc0cd8dd
sakshikadam26 <202401070169@mitaoe.ac.in>
2025-06-18 10:58:32 +0530

    Whileloopexample.java

---
+++ Whileloopexample.java
@@ -0,0 +1,10 @@
+public class Whileloopexample {
+    public static void main(String[] args) {
+        int i = 1; // Initialization
+
+        while (i <= 10) { // Condition
+            System.out.println(i);
+            i++; // Increment
+        }
+    }
+}
