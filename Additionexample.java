3425bcba086569481dbfe34cc02fddb2febebd2f
sakshikadam26 <202401070169@mitaoe.ac.in>
2025-06-18 10:57:06 +0530

    Additionexample.java

---
+++ Additionexample.java
@@ -0,0 +1,18 @@
+public class AdditionExample {
+
+    // Function to subtract two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+
+        // Calling the function and storing result
+        int result = add(num1, num2);
+
+        // Displaying the result
+        System.out.println("Addition Result: " + result);
+    }
+}
